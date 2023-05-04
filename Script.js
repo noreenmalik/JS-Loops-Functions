2366513182dc06fdbe38befd58daab7b0da37bba
noreenmalik <noreenmalik296@gmail.com>
2023-05-04 16:04:06 +0400

    first commit

---
+++ Script.js
@@ -0,0 +1,108 @@
+// using cmd
+// _______________1_______________
+
+// for (var i = 200; i >= 0; i -= 3){
+//       console.log(i);
+//  }
+
+
+// _______________2_______________
+
+//   n = 10;
+//   i= 1;
+// while (i <= n) {
+//     console.log("Noreen")
+//     i++;
+// }
+
+
+// _______________3_______________
+
+
+// let even = 1;
+// let num = 10;
+// let sum = 0;
+// while(even <= num){
+//     if(even % 2 == 0){
+//         sum = sum + even;
+//     }
+//     even++;
+// }
+// console.log("The sum of even numbers from 1 - 10 is:", sum );
+
+
+// _______________4_______________
+
+
+// let number = 1;
+// do {
+//    console.log(number);
+//    number = number + 1;
+// } while (number <=5)
+
+
+// _______________5_______________
+ 
+
+
+//   function add(a, b) {
+//     console.log("The sum of two numbers is ", a + b);
+// }
+
+// add(2, 9);
+
+// _______________6_______________
+
+
+
+// function areaOfrec(width, height) {
+
+//     var area = (width * height);
+//     console.log("The Area of a Rectangle is:", area);
+// }
+
+// var avg = areaOfrec(30, 10);
+
+// for (let index = 1; index <=10; index++) {
+//     console.log('Table of', index);
+
+//    for(let tOf = 1; tOf <=10; tOf++){
+//     console.log(tOf + "*" + index + "=" + index * tOf);
+//    }
+
+    
+// }
+
+
+
+
+
+
+
+
+
+// Practice
+
+// let a = "JavaScript";
+// let b = 6;
+// console.log(a + b);
+// console.log(typeof a)
+
+
+// let marks = {
+//     Ali: 100,
+//     monika: 30,
+//     Sara:40
+// }
+// for (let i = 0; i < Object.keys(marks).length; i++) {
+//     console.log("The marks of" + Object.keys(marks)[i] +"is" + marks[Object.keys(marks)[i]]);
+    
+// }
+// let cn = 43;
+// let i; 
+// while (i!=cn) {
+//     console.log("Try again")
+//     i = prompt("Enter a number")
+// }
+
+// console.log("harr\"".length)
